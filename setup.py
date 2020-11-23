fb898b17b62e078b08ab7fa42455541be991dd80
Ivania Donoso <ivania.donoso@gmail.com>
2020-11-23 16:59:00 -0300

    Updated version of DKN

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='DKN',
-    version='0.1.7',
+    version='0.2',
     url='https://github.com/indonoso/DKN.git',
     author='Ivania Donoso-Guzmán based on fork',
     description='DKN using BERT',
