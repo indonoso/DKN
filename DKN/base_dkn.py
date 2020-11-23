fcb6922439d3100ee75102e9c30928df85a0e325
Ivania Donoso <ivania.donoso@gmail.com>
2020-11-23 16:31:01 -0300

    Changed args tuple to kwargs in DKN

--- DKN/base_dkn.py
+++ DKN/base_dkn.py
@@ -4,15 +4,52 @@ from sklearn.metrics import roc_auc_score
 
 
 class DKN:
-    def __init__(self, args):
-        self.params = []  # for computing regularization loss
-        self._build_inputs(args)
-        self._build_model(args)
-        self._build_train(args)
+    def __init__(self, transform=False, user_bert_embeddings=None, word_embeddings_path=None,
+                 entity_embeddings_path=None,context_embeddings_path=None, use_context=False,max_click_history=10,
+                 max_text_length=10, entity_dim=32, word_dim=32, l2_weight=0.01, filter_sizes=(1, 2), n_filters=128,
+                 lr=0.001, batch_size=128, n_epochs=10):
 
-    def _prepare_data_attention(self, args):
-        clicked_words = tf.reshape(self.clicked_words, shape=[-1, args.max_title_length])
-        clicked_entities = tf.reshape(self.clicked_entities, shape=[-1, args.max_title_length])
+        # Embeddings
+        self.transform = transform
+
+        # Word Embeddings
+        self.user_bert_embeddings = user_bert_embeddings
+        self.word_embeddings_path = word_embeddings_path
+        if not(self.user_bert_embeddings and self.word_embeddings_path):
+            raise ValueError('Neither bert embeddings or Word2Vec has been set')
+
+        # Entity
+        self.entity_embeddings_path = entity_embeddings_path
+
+        # Context
+        self.context_embeddings_path = context_embeddings_path
+        self.use_context = use_context
+
+        # Tensor size
+        self.max_click_history = max_click_history
+        self.max_text_length = max_text_length
+        self.entity_dim = entity_dim
+        self.word_dim = word_dim
+
+        # Model
+        self.l2_weight = l2_weight
+        self.filter_sizes = filter_sizes
+        self.n_filters = n_filters
+        self.lr = lr
+
+        # Training
+
+        self.batch_size = batch_size
+        self.n_epochs = n_epochs
+
+        self.params = []  # for computing regularization loss
+        self._build_inputs()
+        self._build_model()
+        self._build_train()
+
+    def _prepare_data_attention(self):
+        clicked_words = tf.reshape(self.clicked_words, shape=[-1, self.max_text_length])
+        clicked_entities = tf.reshape(self.clicked_entities, shape=[-1, self.max_text_length])
         return clicked_words, clicked_entities
 
     def _prepare_data_kcnn(self, words, entities):
@@ -20,59 +57,59 @@ class DKN:
         embedded_entities = tf.nn.embedding_lookup(params=self.entity_embeddings, ids=entities)
         return embedded_words, embedded_entities
 
-    def _build_inputs(self, args):
+    def _build_inputs(self):
         with tf.compat.v1.name_scope('input'):
             self.clicked_words = tf.compat.v1.placeholder(
-                dtype=tf.int32, shape=[None, args.max_click_history, args.max_title_length], name='clicked_words')
+                dtype=tf.int32, shape=[None, self.max_click_history, self.max_text_length], name='clicked_words')
             self.clicked_entities = tf.compat.v1.placeholder(
-                dtype=tf.int32, shape=[None, args.max_click_history, args.max_title_length], name='clicked_entities')
+                dtype=tf.int32, shape=[None, self.max_click_history, self.max_text_length], name='clicked_entities')
             self.words = tf.compat.v1.placeholder(
-                dtype=tf.int32, shape=[None, args.max_title_length], name='words')
+                dtype=tf.int32, shape=[None, self.max_text_length], name='words')
             self.entities = tf.compat.v1.placeholder(
-                dtype=tf.int32, shape=[None, args.max_title_length], name='entities')
+                dtype=tf.int32, shape=[None, self.max_text_length], name='entities')
             self.labels = tf.compat.v1.placeholder(
                 dtype=tf.float32, shape=[None], name='labels')
 
-    def _build_model(self, args):
+    def _build_model(self):
         with tf.compat.v1.name_scope('embedding'):
-            self.word_embeddings = tf.Variable(np.load(args.word_embeddings), dtype=np.float32, name='word')
-            self.entity_embeddings = tf.Variable(np.load(args.entity_embeddings), dtype=np.float32, name='entity')
+            self.word_embeddings = tf.Variable(np.load(self.word_embeddings_path), dtype=np.float32, name='word')
+            self.entity_embeddings = tf.Variable(np.load(self.entity_embeddings_path), dtype=np.float32, name='entity')
             self.params.append(self.word_embeddings)
             self.params.append(self.entity_embeddings)
 
-            if args.use_context:
-                context_embs = np.load(args.context_embeddings)
+            if self.use_context:
+                context_embs = np.load(self.context_embeddings)
                 self.context_embeddings = tf.Variable(context_embs, dtype=np.float32, name='context')
                 self.params.append(self.context_embeddings)
 
-            if args.transform:
+            if self.transform:
                 self.entity_embeddings = tf.compat.v1.layers.dense(
-                    self.entity_embeddings, units=args.entity_dim, activation=tf.nn.tanh, name='transformed_entity',
-                    kernel_regularizer=tf.keras.regularizers.l2(0.5 * (args.l2_weight)))
-                if args.use_context:
+                    self.entity_embeddings, units=self.entity_dim, activation=tf.nn.tanh, name='transformed_entity',
+                    kernel_regularizer=tf.keras.regularizers.l2(0.5 * self.l2_weight))
+                if self.use_context:
                     self.context_embeddings = tf.compat.v1.layers.dense(
-                        self.context_embeddings, units=args.entity_dim, activation=tf.nn.tanh,
-                        name='transformed_context', kernel_regularizer=tf.keras.regularizers.l2(0.5 * (args.l2_weight)))
+                        self.context_embeddings, units=self.entity_dim, activation=tf.nn.tanh,
+                        name='transformed_context', kernel_regularizer=tf.keras.regularizers.l2(0.5 * self.l2_weight))
 
-        user_embeddings, item_embeddings = self._attention(args)
+        user_embeddings, item_embeddings = self._attention()
         self.scores_unnormalized = tf.reduce_sum(input_tensor=user_embeddings * item_embeddings, axis=1)
         self.scores = tf.sigmoid(self.scores_unnormalized)
 
-    def _attention(self, args):
+    def _attention(self):
         # (batch_size * max_click_history, max_title_length)
-        clicked_words, clicked_entities = self._prepare_data_attention(args)
+        clicked_words, clicked_entities = self._prepare_data_attention()
 
         with tf.compat.v1.variable_scope('kcnn', reuse=tf.compat.v1.AUTO_REUSE):  # reuse the variables of KCNN
             # (batch_size * max_click_history, title_embedding_length)
             # title_embedding_length = n_filters_for_each_size * n_filter_sizes
-            clicked_embeddings = self._kcnn(clicked_words, clicked_entities, args)
+            clicked_embeddings = self._kcnn(clicked_words, clicked_entities)
 
             # (batch_size, title_embedding_length)
-            item_embeddings = self._kcnn(self.words, self.entities, args)
+            item_embeddings = self._kcnn(self.words, self.entities)
 
         # (batch_size, max_click_history, title_embedding_length)
         clicked_embeddings = tf.reshape(
-            clicked_embeddings, shape=[-1, args.max_click_history, args.n_filters * len(args.filter_sizes)])
+            clicked_embeddings, shape=[-1, self.max_click_history, self.n_filters * len(self.filter_sizes)])
 
         # (batch_size, 1, title_embedding_length)
         item_embeddings_expanded = tf.expand_dims(item_embeddings, 1)
@@ -91,30 +128,30 @@ class DKN:
 
         return user_embeddings, item_embeddings
 
-    def _kcnn(self, words, entities, args):
+    def _kcnn(self, words, entities):
         # (batch_size * max_click_history, max_title_length, word_dim) for users
         # (batch_size, max_title_length, word_dim) for items
         embedded_words, embedded_entities = self._prepare_data_kcnn(words, entities)
 
         # (batch_size * max_click_history, max_title_length, full_dim) for users
         # (batch_size, max_title_length, full_dim) for items
-        if args.use_context:
+        if self.use_context:
             embedded_contexts = tf.nn.embedding_lookup(params=self.context_embeddings, ids=entities)
             concat_input = tf.concat([embedded_words, embedded_entities, embedded_contexts], axis=-1)
-            full_dim = args.word_dim + args.entity_dim * 2
+            full_dim = self.word_dim + self.entity_dim * 2
         else:
             concat_input = tf.concat([embedded_words, embedded_entities], axis=-1)
-            full_dim = args.word_dim + args.entity_dim
+            full_dim = self.word_dim + self.entity_dim
 
         # (batch_size * max_click_history, max_title_length, full_dim, 1) for users
         # (batch_size, max_title_length, full_dim, 1) for items
         concat_input = tf.expand_dims(concat_input, -1)
 
         outputs = []
-        for filter_size in args.filter_sizes:
-            filter_shape = [filter_size, full_dim, 1, args.n_filters]
+        for filter_size in self.filter_sizes:
+            filter_shape = [filter_size, full_dim, 1, self.n_filters]
             w = tf.compat.v1.get_variable(name='w_' + str(filter_size), shape=filter_shape, dtype=tf.float32)
-            b = tf.compat.v1.get_variable(name='b_' + str(filter_size), shape=[args.n_filters], dtype=tf.float32)
+            b = tf.compat.v1.get_variable(name='b_' + str(filter_size), shape=[self.n_filters], dtype=tf.float32)
             if w not in self.params:
                 self.params.append(w)
 
@@ -125,8 +162,8 @@ class DKN:
 
             # (batch_size * max_click_history, 1, 1, n_filters_for_each_size) for users
             # (batch_size, 1, 1, n_filters_for_each_size) for items
-            pool = tf.nn.max_pool2d(input=relu, ksize=[1, args.max_title_length - filter_size + 1, 1, 1],
-                                  strides=[1, 1, 1, 1], padding='VALID', name='pool')
+            pool = tf.nn.max_pool2d(input=relu, ksize=[1, self.max_text_length - filter_size + 1, 1, 1],
+                                    strides=[1, 1, 1, 1], padding='VALID', name='pool')
             outputs.append(pool)
 
         # (batch_size * max_click_history, 1, 1, n_filters_for_each_size * n_filter_sizes) for users
@@ -135,21 +172,21 @@ class DKN:
 
         # (batch_size * max_click_history, n_filters_for_each_size * n_filter_sizes) for users
         # (batch_size, n_filters_for_each_size * n_filter_sizes) for items
-        output = tf.reshape(output, [-1, args.n_filters * len(args.filter_sizes)])
+        output = tf.reshape(output, [-1, self.n_filters * len(self.filter_sizes)])
 
         return output
 
-    def _build_train(self, args):
+    def _build_train(self):
         with tf.compat.v1.name_scope('train'):
             self.base_loss = tf.reduce_mean(
                 input_tensor=tf.nn.sigmoid_cross_entropy_with_logits(labels=self.labels, logits=self.scores_unnormalized))
             self.l2_loss = tf.Variable(tf.constant(0., dtype=tf.float32), trainable=False)
             for param in self.params:
-                self.l2_loss = tf.add(self.l2_loss, args.l2_weight * tf.nn.l2_loss(param))
-            if args.transform:
+                self.l2_loss = tf.add(self.l2_loss, self.l2_weight * tf.nn.l2_loss(param))
+            if self.transform:
                 self.l2_loss = tf.add(self.l2_loss, tf.compat.v1.losses.get_regularization_loss())
             self.loss = self.base_loss + self.l2_loss
-            self.optimizer = tf.compat.v1.train.AdamOptimizer(args.lr).minimize(self.loss)
+            self.optimizer = tf.compat.v1.train.AdamOptimizer(self.lr).minimize(self.loss)
 
     def train(self, sess, feed_dict):
         return sess.run(self.optimizer, feed_dict)
---
+++ DKN/dknbert.py
@@ -0,0 +1,60 @@
+import numpy as np
+import tensorflow as tf
+from transformers import AutoModel, AutoTokenizer
+from .pipeline import CachedFeatureExtractionPipeline
+from .base_dkn import DKN
+
+
+class DKNBert(DKN):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+        model = AutoModel.from_pretrained('allenai/scibert_scivocab_uncased')
+        self.scibert = CachedFeatureExtractionPipeline(self.word_dim, self.max_text_length, model, tokenizer,
+                                                       task='word_embeddings')
+
+    def _build_inputs(self):
+        with tf.compat.v1.name_scope('input'):
+            self.clicked_words = tf.compat.v1.placeholder(
+                dtype=tf.float32, shape=[None, self.max_click_history, self.max_text_length, self.word_dim],
+                name='clicked_words')
+            self.clicked_entities = tf.compat.v1.placeholder(
+                dtype=tf.int32, shape=[None, self.max_click_history, self.max_text_length], name='clicked_entities')
+            self.words = tf.compat.v1.placeholder(
+                dtype=tf.float32, shape=[None, self.max_text_length, self.word_dim], name='words')
+            self.entities = tf.compat.v1.placeholder(
+                dtype=tf.int32, shape=[None, self.max_text_length], name='entities')
+            self.labels = tf.compat.v1.placeholder(
+                dtype=tf.float32, shape=[None], name='labels')
+
+    def _build_model(self):
+        with tf.compat.v1.name_scope('embedding'):
+            self.entity_embeddings = tf.Variable(np.load(self.entity_embeddings), dtype=np.float32, name='entity')
+            self.params.append(self.entity_embeddings)
+
+            if self.use_context:
+                context_embs = np.load(self.context_embeddings)
+                self.context_embeddings = tf.Variable(context_embs, dtype=np.float32, name='context')
+                self.params.append(self.context_embeddings)
+
+            if self.transform:
+                self.entity_embeddings = tf.compat.v1.layers.dense(
+                    self.entity_embeddings, units=self.entity_dim, activation=tf.nn.tanh, name='transformed_entity',
+                    kernel_regularizer=tf.keras.regularizers.l2(0.5 * self.l2_weight))
+                if self.use_context:
+                    self.context_embeddings = tf.compat.v1.layers.dense(
+                        self.context_embeddings, units=self.entity_dim, activation=tf.nn.tanh,
+                        name='transformed_context', kernel_regularizer=tf.keras.regularizers.l2(0.5 * self.l2_weight))
+
+        user_embeddings, item_embeddings = self._attention()
+        self.scores_unnormalized = tf.reduce_sum(input_tensor=user_embeddings * item_embeddings, axis=1)
+        self.scores = tf.sigmoid(self.scores_unnormalized)
+
+    def _prepare_data_attention(self):
+        clicked_words = tf.reshape(self.clicked_words, shape=[-1, self.max_text_length, self.word_dim])
+        clicked_entities = tf.reshape(self.clicked_entities, shape=[-1, self.max_text_length])
+        return clicked_words, clicked_entities
+
+    def _prepare_data_kcnn(self, words, entities):
+        embedded_entities = tf.nn.embedding_lookup(params=self.entity_embeddings, ids=entities)
+        return words, embedded_entities
